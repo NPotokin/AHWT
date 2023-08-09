c95d9091f25e816879f3fbe13a49590971dd7e97
NPotokin <npotokin@gmail.com>
2023-08-09 16:16:43 -0600

    initial set up

---
+++ client/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#c084fc",
+          "secondary": "#5eead4",
+          "accent": "#fde047",
+          "neutral": "#121c21",
+          "base-100": "#1e1b4b",
+          "info": "#a5f3fc",
+          "success": "#a7f3d0",
+          "warning": "#fef08a",
+          "error": "#fecaca",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
+
