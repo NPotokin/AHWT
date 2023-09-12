1682a32adbfa134861669558e8f8b32372eaa6d3
NPotokin <npotokin@gmail.com>
2023-09-12 11:43:55 -0600

    working on hospital page

--- client/components/Hospital.jsx
+++ client/components/Hospital.jsx
@@ -1,26 +1,67 @@
 import React from 'react'
+import {GoLocation} from 'react-icons/go'
+import {FiPhone} from 'react-icons/fi'
+import {CgWebsite} from 'react-icons/cg'
 
 const Hospital = (props) => {
   return (
     <div className='mx-3'>
+
       <div className='container flex flex-col'>
-        <div>
-          {props.name}
-        </div>
-
-        <div className=" flex flex-row">
-          <div className="border-primary border-4">{props.img}</div>
-          <div className="border-primary border-4 ">{props.info}</div>
-        </div>
-
-          {/* <div className="w-[300px] h-[300px] border-primary border-4">{props.hourGraph}</div>
-          <div className="w-[300px] h-[300px] border-4 border-primary">{props.dayGraph}</div>
-          <div className="w-[300px] h-[300px] border-4 border-primary">{props.weekGraph}</div>
-          <div className="w-[600px] h-[300px] border-4 border-primary col-span-2">{props.stat}</div>
-          <div className="w-[300px] h-[300px] border-4 border-primary col-span-2">{props.about}</div> */}
-      </div>
       
+        <div className='m-4 py-4 text-4xl lg:text-7xl xl:text-8xl font-bold text-center'>
+          {props.hospitalName}
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto">
+         
+          {/*Contact Info */}
+          <div className="flex flex-col rounded-3xl w-[300px] h-[300px] xl:w-[400px] xl:h-[400px] border-primary border-4">
+            <div className='my-2 font-bold text-2xl xl:text-4xl text-secondary'>Contact Info</div>
+
+            <div className="flex flex-row mx-auto mt-2">
+              <GoLocation className='text-secondary mx-2' size={25} />
+              <div className='font-bold text-lg'>Adress</div>
+            </div>
+            <div className='font-semibold text-md'>{props.street}</div>
+            <div className='font-semibold text-md'>{props.city}</div>
+            <div className='font-semibold text-md'>{props.zip}</div>
+
+            <div className="flex flex-row mx-auto mt-2">
+              <FiPhone className='text-secondary mx-2' size={25} />
+              <div className='font-bold text-lg'>Phone</div>
+            </div>
+            <div className=' text-md'>{props.phone}</div>
+
+            <div className="flex flex-row mx-auto mt-2">
+              <CgWebsite className='text-secondary mx-2' size={25} />
+              <div className='font-bold text-lg'>Website</div>
+            </div>
+            <div className='font-semibold text-md'>{props.website}</div>
+          </div>
+        
+        
+        
+          <div className="rounded-3xl w-[300px] h-[300px] xl:w-[400px] xl:h-[400px] border-4 border-primary">{props.stat}</div>
+          <div className="rounded-3xl w-[300px] h-[300px] xl:w-[400px] xl:h-[400px] border-primary border-4">{props.hourGraph}</div>
+          <div className="rounded-3xl w-[300px] h-[300px] xl:w-[400px] xl:h-[400px] border-4 border-primary">{props.dayGraph}</div>
+          <div className="rounded-3xl w-[300px] h-[300px] xl:w-[400px] xl:h-[400px] border-4 border-primary">{props.weekGraph}</div>
+          <div className="rounded-3xl w-[300px] h-[300px] xl:w-[400px] xl:h-[400px] border-4 border-primary ">{props.aboutInfo}</div>
+        </div>
+          
+          
+          
+          
+          
+          
+          
+          
+        </div>
+
     </div>
+
+      
+    
   )
 }
 
--- client/pages/calgary/AlbertaChildrensHospital.jsx
+++ client/pages/calgary/AlbertaChildrensHospital.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Hospital from '../../components/Hospital'
+import Hospitals from '../../utils/Hospitals'
 
 const AlbertaChildrensHospital = () => {
+
+  const albChildHosp = Hospitals.filter((item) => item.hospitalName === 'Alberta Childrens Hospital')[0];
+  
+
   return (
     <div>
-      Alberta Childrens Hospital
+      
       <Hospital
-        name='hospital name'
-        about='about info'
-        img='some image'
-        info='main info with adrees and phone'
+        hospitalName={albChildHosp.hospitalName}
+        aboutInfo={albChildHosp.aboutInfo}
+        street={albChildHosp.contactInfo.adress.street}
+        city={albChildHosp.contactInfo.adress.city}
+        zip={albChildHosp.contactInfo.adress.zip}
+        phone={albChildHosp.contactInfo.phone}
+        website={albChildHosp.contactInfo.website}
         stat='statistics about the waiting times'
         hourGraph='hourly graph'
         dayGraph='dayly graph'
